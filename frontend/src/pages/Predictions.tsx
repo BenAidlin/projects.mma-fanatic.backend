7b4edefc9d18f0c5312974f0182f764efc7cf337
benaidlin <ben.aidlin@adaptive-shield.com>
2025-01-27 19:34:50 +0200

    fix compose build

--- frontend/src/pages/Predictions.tsx
+++ frontend/src/pages/Predictions.tsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { RootState } from '../store';
-import { Fight, Prediction } from '../types';
+import { Fight, Prediction, Card, Match } from '../types';
+
 
 const Predictions: React.FC = () => {
-  const fights = useSelector((state: RootState) => state.fights);
+  const fightState = useSelector((state: RootState) => state.fights);
   const userPredictions = useSelector((state: RootState) => state.user.predictions);
 
   const findFightAndMatch = (prediction: Prediction): { fight: Fight | undefined, matchup: string } => {
-    const fight = fights.find(f => f.id === prediction.fightId);
+    const fight = fightState.fights.find((f: Fight) => f.id === prediction.fightId);
     if (fight) {
-      const match = fight.cards.flatMap(card => card.mtchs).find(m => 
+      const match = fight.cards.flatMap((card: Card) => card.mtchs).find((m: Match) => 
         m.awy.original_id === prediction.matchId || m.hme.original_id === prediction.matchId
       );
       if (match) {
