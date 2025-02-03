f5508bbcb3a8af22e87b09ef0aa5b4bd3f7ec90e
benaidlin <ben.aidlin@adaptive-shield.com>
2025-02-03 21:20:43 +0200

    make auth work when running against the container

--- frontend/src/services/auth.ts
+++ frontend/src/services/auth.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import Cookies from 'js-cookie';
 
-const API_URL = 'http://localhost:8000';
+const API_URL = 'http://localhost:8004';
 
 export const initiateGoogleLogin = () => {
   window.location.href = `${API_URL}/auth/google`;
